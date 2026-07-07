0cc99cbeb3c4c1ed135ebdf45acb1fef5fe407b6
byyang233 <88868791+byyang233@users.noreply.github.com>
2026-07-07 22:13:44 +0800

    feat: add steam search provider

---
+++ api/providers/steam-search.js
@@ -0,0 +1,7 @@
+window.GameBoxSteamSearch = {
+    async search(keyword) {
+        // Steam search endpoint will be connected here.
+        // Return candidate games with appid.
+        return [];
+    }
+};
